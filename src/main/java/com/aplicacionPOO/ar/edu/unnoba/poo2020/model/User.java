d64676d6d3dcbdef8bcb679e0b0a14568f0930e8
julito <zabalajulian2010@hotmail.com>
2020-10-12 18:12:16 -0300

    desarrollo de la clase user

---
+++ src/main/java/com/aplicacionPOO/ar/edu/unnoba/poo2020/model/User.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aplicacionPOO.ar.edu.unnoba.poo2020.model;
+
+/**
+ *
+ * @author Julito
+ */
+public class User {
+    
+}
